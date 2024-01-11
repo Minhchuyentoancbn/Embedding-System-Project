e6a58ad742af69c2b0e1f9664b20511c2dc0cb31
Le Duc Minh <minhmeo7a1@gmail.com>
2024-01-11 09:06:36 +0700

    Final version

--- pong/pong.ino
+++ pong/pong.ino
@@ -10,113 +10,11 @@
 #include <gfxfont.h>
 #include <EEPROM.h>
 
-/*
-  A simple Pong game.
-  https://notabug.org/Maverick/WokwiPong
-  
-  Based on Arduino Pong by eholk
-  https://github.com/eholk/Arduino-Pong
-*/
-// SCL: D22
-// SDA: D21
-
 #include <SPI.h>
 #include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
 #include <SD.h>
 
 #define SD_PIN 5
-
-#define B0 31
-#define C1 33
-#define CS1 35
-#define D1 37
-#define DS1 39
-#define E1 41
-#define F1 44
-#define FS1 46
-#define G1 49
-#define GS1 52
-#define A1 55
-#define AS1 58
-#define B1 62
-#define C2 65
-#define CS2 69
-#define D2 73
-#define DS2 78
-#define E2 82
-#define F2 87
-#define FS2 93
-#define G2 98
-#define GS2 104
-#define A2 110
-#define AS2 117
-#define B2 123
-#define C3 131
-#define CS3 139
-#define D3 147
-#define DS3 156
-#define E3 165
-#define F3 175
-#define FS3 185
-#define G3 196
-#define GS3 208
-#define A3 220
-#define AS3 233
-#define B3 247
-#define C4 262
-#define CS4 277
-#define D4 294
-#define DS4 311
-#define E4 330
-#define F4 349
-#define FS4 370
-#define G4 392
-#define GS4 415
-#define A4 440
-#define AS4 466
-#define B4 494
-#define C5 523
-#define CS5 554
-#define D5 587
-#define DS5 622
-#define E5 659
-#define F5 698
-#define FS5 740
-#define G5 784
-#define GS5 831
-#define A5 880
-#define AS5 932
-#define B5 988
-#define C6 1047
-#define CS6 1109
-#define D6 1175
-#define DS6 1245
-#define E6 1319
-#define F6 1397
-#define FS6 1480
-#define G6 1568
-#define GS6 1661
-#define A6 1760
-#define AS6 1865
-#define B6 1976
-#define C7 2093
-#define CS7 2217
-#define D7 2349
-#define DS7 2489
-#define E7 2637
-#define F7 2794
-#define FS7 2960
-#define G7 3136
-#define GS7 3322
-#define A7 3520
-#define AS7 3729
-#define B7 3951
-#define C8 4186
-#define CS8 4435
-#define D8 4699
-#define DS8 4978
 
 #define UP_BUTTON 26
 #define DOWN_BUTTON 27
@@ -124,15 +22,10 @@
 #define UP_BUTTON_MCU 12
 #define DOWN_BUTTON_MCU 13
 
-#define H 175
-#define B H * 2
-#define W B * 2
-
 const unsigned long PADDLE_RATE = 64;
-
-const unsigned long BALL_RATE = 10;
+const unsigned long BALL_RATE = 10;  // 10ms
 const uint8_t PADDLE_HEIGHT = 12;
-const uint8_t SCORE_LIMIT = 2;
+const uint8_t SCORE_LIMIT = 6;
 
 Adafruit_SSD1306 display = Adafruit_SSD1306(128, 64, &Wire);
 
@@ -140,7 +33,7 @@ bool game_over, win;
 
 uint8_t player_score, mcu_score;
 uint8_t ball_x = 53, ball_y = 26;
-uint8_t ball_dir_x = 1, ball_dir_y = 1;
+uint8_t ball_dir_x = 1, ball_dir_y = 1;  
 
 unsigned long ball_update;
 unsigned long paddle_player_update, paddle_mcu_update;
@@ -152,13 +45,9 @@ const uint8_t PLAYER_X = 115;
 uint8_t player_y = 16;
 
 // notes in the melody:
-int winning_melody[] = {
-  D4, G4, G4, A4, G4, FS4, E4, E4, E4, A4, A4, B4, A4, G4, FS4, D4, D4, B4, B4, C5, B4, A4, G4, E4, D4, D4, E4, A4, FS4, G4
-};
+int winning_melody[30];
 // note durations: 4 = quarter note, 8 = eighth note, etc.:
-int winning_noteDurations[] = {
-  B, B, H, H, H, H, B, B, B, B, H, H, H, H, B, B, B, B, H, H, H, H, B, B, H, H, B, B, B, W
-};
+int winning_noteDurations[30];
 
 void changetune(int melody[], int duration[]) {
   for (int i = 0; i < 30; ++i) {
@@ -221,17 +110,13 @@ void setup() {
 
   pinMode(UP_BUTTON, INPUT_PULLUP);
   pinMode(DOWN_BUTTON, INPUT_PULLUP);
-
   pinMode(UP_BUTTON_MCU, INPUT_PULLUP);
   pinMode(DOWN_BUTTON_MCU, INPUT_PULLUP);
 
-  select_tune();
-
-
+  select_tune();  // Select tune before starting game
 
   display.clearDisplay();
   drawCourt();
-
   display.display();
 
   ball_update = millis();
@@ -316,6 +201,8 @@ void select_tune() {
     delay(10);
   }
 }
+
+
 void loop() {
   bool update_needed = false;
 
@@ -323,13 +210,11 @@ void loop() {
 
   static bool up_state = false;
   static bool down_state = false;
-
   static bool up_state_mcu = false;
   static bool down_state_mcu = false;
 
   up_state |= (digitalRead(UP_BUTTON) == LOW);
   down_state |= (digitalRead(DOWN_BUTTON) == LOW);
-
   up_state_mcu |= (digitalRead(UP_BUTTON_MCU) == LOW);
   down_state_mcu |= (digitalRead(DOWN_BUTTON_MCU) == LOW);
 
@@ -455,9 +340,9 @@ void loop() {
     if (game_over) {
       String text;
       if (win) {
-        text = "Player 2 Won\n\n        Score " + String(mcu_score) + " - " + String(player_score);
+        text = "Player 2 Won\n\n       Score " + String(mcu_score) + " - " + String(player_score);
       } else {
-        text = "Player 1 Won\n\n        Score " + String(mcu_score) + " - " + String(player_score);
+        text = "Player 1 Won\n\n       Score " + String(mcu_score) + " - " + String(player_score);
       }
       // const char* text = win ? "YOU WIN!!" : "YOU LOSE!";
       display.clearDisplay();
